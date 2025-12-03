3d0fddfe5a669136cf7752a5942890da2e5923ed
Jonathan Fraser <jonathanfras@gmail.com>
2025-12-02 21:40:03 -0800

    get rid of some warnings Day1

--- src/Day1.hs
+++ src/Day1.hs
@@ -3,11 +3,14 @@
 {-# LANGUAGE OverloadedStrings #-}
 module Day1 (run_parts) where
 
-import qualified Data.Text.Encoding as E
-import qualified Data.ByteString as BS
 import qualified Data.Text as T
 import Data.FileEmbed (embedFile)
 import qualified Lib as L
+import Control.Exception (throw, Exception)
+
+data InvalidInput = InvalidInput String deriving (Show)
+
+instance Exception InvalidInput
 
 fileLines :: [T.Text]
 fileLines = T.lines $ L.decodeTextFile $(embedFile "data/2025_day1_p1.txt")
@@ -15,14 +18,15 @@ fileLines = T.lines $ L.decodeTextFile $(embedFile "data/2025_day1_p1.txt")
 processLine :: String -> Int  
 processLine ('R':x) = read x
 processLine ('L':x) = negate (read x)
+processLine _ = throw (InvalidInput "Invalid input")
 
 processLine2 :: String -> [Int]  
 processLine2 ('R':x) = take (read x) (repeat 1)
 processLine2 ('L':x) = take (read x) (repeat (-1))
+processLine2 _ = throw (InvalidInput "Invalid input")
 
 increment :: Int -> Int -> Int
 increment original step = (original + step) `mod` 100
-
 
 part1 :: Int
 part1 = let 
@@ -32,8 +36,8 @@ part1 = let
 
 part2 :: Int
 part2 = let 
-            steps = map (processLine2 . T.unpack) fileLines
-            result = scanl increment 50 (concat steps)
+            steps = fileLines >>= (processLine2 . T.unpack)
+            result = scanl increment 50 steps
         in length (filter (==0) result)
 
 run_parts :: IO ()
